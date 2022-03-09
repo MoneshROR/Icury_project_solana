3e9cc328d96866b86f138b1963e94049d54be57e
MoneshROR <monesh.thoughtwin@gmail.com>
2022-03-09 13:46:52 +0530

    create icury token program

--- programs/create_icury_token/src/lib.rs
+++ programs/create_icury_token/src/lib.rs
@@ -1,15 +1,24 @@
-use anchor_lang::prelude::*;
+// use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+// declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
-#[program]
-pub mod create_icury_token {
-    use super::*;
+// #[program]
+// pub mod create_icury_token {
+//     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
-}
+//     pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+//         Ok(())
+//     }
+// }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+// #[derive(Accounts)]
+// pub struct Initialize {}
+
+#[cfg(feature = "associated_token")]
+pub mod associated_token;
+
+#[cfg(feature = "mint")]
+pub mod mint;
+
+#[cfg(feature = "token")]
+pub mod token;
